f14a502e08934742860b40b91dee69fd8872bea5
dremangry <andrezreyes2015@gmail.com>
2022-06-26 19:01:21 -0400

    creating rate converter

--- src/components/CurrencyConverter.js
+++ src/components/CurrencyConverter.js
@@ -1,10 +1,53 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ExchangeRate from "./ExchangeRate";
+import axios from 'axios'
 
 function CurrencyConverter() {
     // array of currency
     const currencies = ['BTC', 'ETH', 'USD', 'XRP', 'LTC', 'ADA']
 
+        // chosenPrimaryCurrency start at null for the first select
+    const [chosenPrimaryCurrency, setChosenPrimaryCurrency] = useState('BTC')
+        //for the second select
+    const [chosenSecondaryCurrency, setChosenSecondaryCurrency] = useState('BTC')
+        // for the number in the input form
+    const [amount, setAmount] = useState(1)
+        // for the exchange rate data
+    const [exchangeRate, setExchangeRate] = useState(0)
+        //for the exchange rate data multiplied by the amount
+    const [result, setResult] = useState(0)
+    
+    console.log(chosenPrimaryCurrency, 'first');
+    console.log(chosenSecondaryCurrency, 'second');
+    console.log(amount);
+    
+    function convert() {
+        
+        const options = {
+            method: 'GET',
+            url: 'https://alpha-vantage.p.rapidapi.com/query',
+            params: { from_currency: chosenPrimaryCurrency, function: 'CURRENCY_EXCHANGE_RATE', to_currency: chosenSecondaryCurrency },
+            headers: {
+                
+            }
+        };
+        
+        axios.request(options).then(function (response) {
+            console.log('the hole object=====================');
+            console.log(response.data);
+            console.log('the hole object=====================');
+            // go into the api object and inside realtime exchange currency rate ,5. Exchange Rate and set it into exchangeRate
+            console.log(response.data['Realtime Currency Exchange Rate']['5. Exchange Rate']);
+            setExchangeRate(response.data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
+                //for the exchange rate data multiplied by the amount
+            setResult(response.data['Realtime Currency Exchange Rate']['5. Exchange Rate'] * amount)
+        }).catch(function (error) {
+            console.error(error);
+        });
+    }
+    console.log('####################################');
+    console.log(exchangeRate);
+    console.log('####################################');
     return (
         <div className='currency-converter'>
             <h2>Currency Converter</h2>
@@ -18,14 +61,16 @@ function CurrencyConverter() {
                                 <input
                                     type="number"
                                     name='currency-amount-1'
-                                    value={""}
+                                    value={amount}
+                                    onChange={(e) => setAmount(e.target.value)}
                                 />
                             </td>
                             <td>
                                 <select
                                     name="currency-option-1"
                                     className='currency-options'
-                                    value={''}
+                                    value={chosenPrimaryCurrency}
+                                    onChange={(e) => setChosenPrimaryCurrency(e.target.value)}
                                     >
                                         {/* map the array of currencies inside the option (index no needed) */}
                                     {currencies.map((currency, _index) => (<option key={_index}>{currency}</option>))}
@@ -41,14 +86,16 @@ function CurrencyConverter() {
                                 <input
                                     type="number"
                                     name='currency-amount-2'
-                                    value={""}
+                                    value={result}
+                                    disabled={true}
                                 />
                             </td>
                             <td>
                                 <select
                                     name="currency-option-2"
                                     className='currency-options'
-                                    value={''}
+                                    value={chosenSecondaryCurrency}
+                                    onChange={(e) => setChosenSecondaryCurrency(e.target.value)}
                                     >
                                     {currencies.map((currency, _index) => (<option key={_index}>{currency}</option>))}
 
@@ -57,6 +104,8 @@ function CurrencyConverter() {
                         </tr>
                     </tbody>
                 </table>
+                {/* you can see the api call inside the browser developer tool */}
+                <button onClick={convert}>Convert</button>
             </div>
 
             <ExchangeRate />
